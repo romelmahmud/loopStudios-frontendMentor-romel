c2acc8c6d8bec9a14e955c48ebb736af199191a7
romelmahmud <romelmahmud@outlook.com>
2021-02-06 17:54:35 +0600

    change icon hamburger to close when click set

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 const burger = document.querySelector(".nav__hamBurger");
 const mobileLinks = document.querySelector(".nav__mobileLinks");
 const body = document.querySelector("body");
+// buger icon attribute
+
+// console.log(burger.children[0].);
 
 burger.addEventListener("click", () => {
   if (mobileLinks.classList.contains("show")) {
@@ -8,11 +11,13 @@ burger.addEventListener("click", () => {
     mobileLinks.classList.add("hidden");
     body.classList.add("scroll");
     body.classList.remove("no-scroll");
+    burger.children[0].setAttribute("src", "/images/icon-hamburger.svg");
   } else {
     mobileLinks.classList.add("show");
     mobileLinks.classList.remove("hidden");
     body.classList.remove("scroll");
     body.classList.add("no-scroll");
+    burger.children[0].setAttribute("src", "/images/icon-close.svg");
   }
 });
 console.log(mobileLinks.classList);
