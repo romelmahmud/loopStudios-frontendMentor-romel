d7fb6502a89a4d85b87894ad5b18201c0f5414c1
romelmahmud <romelmahmud@outlook.com>
2021-02-06 17:45:20 +0600

    scroll hidden when mobile manu showing addd

--- js/app.js
+++ js/app.js
@@ -1,13 +1,18 @@
 const burger = document.querySelector(".nav__hamBurger");
 const mobileLinks = document.querySelector(".nav__mobileLinks");
+const body = document.querySelector("body");
 
 burger.addEventListener("click", () => {
   if (mobileLinks.classList.contains("show")) {
     mobileLinks.classList.remove("show");
     mobileLinks.classList.add("hidden");
+    body.classList.add("scroll");
+    body.classList.remove("no-scroll");
   } else {
     mobileLinks.classList.add("show");
     mobileLinks.classList.remove("hidden");
+    body.classList.remove("scroll");
+    body.classList.add("no-scroll");
   }
 });
 console.log(mobileLinks.classList);
