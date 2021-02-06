cd527d39a48d8eeaafad58f40aa573a48b8bd548
romelmahmud <romelmahmud@outlook.com>
2021-02-06 19:10:32 +0600

    smooth scroll added and disable mobile menu when mobile links clicked

--- js/app.js
+++ js/app.js
@@ -1,23 +1,37 @@
 const burger = document.querySelector(".nav__hamBurger");
 const mobileLinks = document.querySelector(".nav__mobileLinks");
 const body = document.querySelector("body");
-// buger icon attribute
 
-// console.log(burger.children[0].);
+// toggle function
 
-burger.addEventListener("click", () => {
+const toggle = () => {
   if (mobileLinks.classList.contains("show")) {
     mobileLinks.classList.remove("show");
     mobileLinks.classList.add("hidden");
+    //add/remove scroll
     body.classList.add("scroll");
     body.classList.remove("no-scroll");
+    //changing close icon to buger icon
     burger.children[0].setAttribute("src", "/images/icon-hamburger.svg");
   } else {
     mobileLinks.classList.add("show");
     mobileLinks.classList.remove("hidden");
+    //add/remove scroll
     body.classList.remove("scroll");
     body.classList.add("no-scroll");
+    //changing burger icon to close icon
     burger.children[0].setAttribute("src", "/images/icon-close.svg");
   }
-});
-console.log(mobileLinks.classList);
+  return toggle;
+};
+
+// displaing mobile menu
+burger.addEventListener("click", toggle());
+
+// disable mobile menu when link clicked
+
+const links = document.querySelectorAll(".nav__mobileLinks a");
+
+const linkList = Array.from(links);
+
+linkList.map((link) => link.addEventListener("click", toggle()));
